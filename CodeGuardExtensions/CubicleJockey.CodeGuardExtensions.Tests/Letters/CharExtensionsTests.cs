d5ace44086f0ca80f9b007b1b7b034a267e777a2
André Davis <CubicleJockey@users.noreply.github.com>
2016-10-26 19:02:04 -0700

    Started CharExtensions and Tests

---
+++ CodeGuardExtensions/CubicleJockey.CodeGuardExtensions.Tests/Letters/CharExtensionsTests.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.ComponentModel;
+using CubicleJockey.CodeGuardExtensions;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Seterlund.CodeGuard;
+
+namespace CubicleJockey.CodeGuardExtentions.Tests.Letters
+{
+    [TestClass]
+    public class CharExtensionsTests : BaseTest
+    {
+        #region IsEqual
+
+        [TestMethod]
+        public void IsEqualCustomMessage()
+        {
+            const char VALUE = 'E';
+            var result = Guard.That(VALUE).IsEqual('E', "Should not see me.").Value;
+            result.ShouldBeEquivalentTo(VALUE);
+        }
+
+        [TestMethod]
+        public void IsEqualCustomMessageFail()
+        {
+            const string MESSAGE = "Not Equal and shit.";
+            Action check = () => Guard.That('z').IsEqual('Z', MESSAGE);
+
+            check.ShouldThrow<ArgumentException>()
+                .WithMessage(MESSAGE);
+        }
+
+        [TestMethod]
+        public void IsEqualCustomMessageIsNull()
+        {
+            Action check = () => Guard.That('A').IsEqual('C', null);
+
+            check.ShouldThrow<ArgumentException>()
+                .WithMessage(ExpectedCustomeInvalidErrorMessage("IsEqual"));
+        }
+
+        [TestMethod]
+        public void IsEqualCustomMessageIsEmptyString()
+        {
+            Action check = () => Guard.That('A').IsEqual('C', string.Empty);
+
+            check.ShouldThrow<ArgumentException>()
+                .WithMessage(ExpectedCustomeInvalidErrorMessage("IsEqual"));
+        }
+
+        [TestMethod]
+        public void IsEqualCustomMessageIsWhitespace()
+        {
+            Action check = () => Guard.That('A').IsEqual('C', "  ");
+
+            check.ShouldThrow<ArgumentException>()
+                .WithMessage(ExpectedCustomeInvalidErrorMessage("IsEqual"));
+        }
+
+        #endregion IsEqual
+
+        #region IsNotEqual
+
+        [TestMethod]
+        public void IsNotEqualCustomMessage()
+        {
+            const char VALUE = 'E';
+            var result = Guard.That(VALUE).IsNotEqual('Z', "Should not see me.").Value;
+            result.ShouldBeEquivalentTo(VALUE);
+        }
+
+        [TestMethod]
+        public void IsNotEqualCustomMessageFail()
+        {
+            const string MESSAGE = "Equal and shit.";
+            Action check = () => Guard.That('Z').IsNotEqual('Z', MESSAGE);
+
+            check.ShouldThrow<ArgumentException>()
+                .WithMessage(MESSAGE);
+        }
+
+        [TestMethod]
+        public void IsNotEqualCustomMessageIsNull()
+        {
+            Action check = () => Guard.That('A').IsNotEqual('C', null);
+
+            check.ShouldThrow<ArgumentException>()
+                .WithMessage(ExpectedCustomeInvalidErrorMessage("IsNotEqual"));
+        }
+
+        [TestMethod]
+        public void IsNotEqualCustomMessageIsEmptyString()
+        {
+            Action check = () => Guard.That('A').IsNotEqual('C', string.Empty);
+
+            check.ShouldThrow<ArgumentException>()
+                .WithMessage(ExpectedCustomeInvalidErrorMessage("IsNotEqual"));
+        }
+
+        [TestMethod]
+        public void IsNotEqualCustomMessageIsWhitespace()
+        {
+            Action check = () => Guard.That('A').IsNotEqual('C', "  ");
+
+            check.ShouldThrow<ArgumentException>()
+                .WithMessage(ExpectedCustomeInvalidErrorMessage("IsNotEqual"));
+        }
+
+        #endregion IsNotEqual
+
+        #region IsGreaterThan
+
+        [TestMethod]
+        public void IsGreaterThanCustomMessage()
+        {
+            const char LHS = 'B';
+            const char RHS = 'A';
+
+            var result = Guard.That(LHS).IsGreaterThan(RHS, "Shouldn't see me.").Value;
+
+            result.ShouldBeEquivalentTo(LHS);
+        }
+
+        [TestMethod]
+        public void IsGreaterThanCustomMessageFailed()
+        {
+            const string MESSAGE = "Characters can be greater than, dafauc.";
+
+            const char LHS = 'A';
+            const char RHS = 'B';
+
+            Action check = () => Guard.That(LHS).IsGreaterThan(RHS, MESSAGE);
+
+            check.ShouldThrow<ArgumentException>()
+                .WithMessage(MESSAGE);
+        }
+
+        [TestMethod]
+        public void IsGreaterThanCustomMessageIsNull()
+        {
+            const char LHS = 'A';
+            const char RHS = 'B';
+
+            Action check = () => Guard.That(LHS).IsGreaterThan(RHS, null);
+
+            check.ShouldThrow<ArgumentException>()
+                .WithMessage(ExpectedCustomeInvalidErrorMessage("IsGreaterThan"));
+        }
+
+        [TestMethod]
+        public void IsGreaterThanCustomMessageIsEmptyString()
+        {
+            const char LHS = 'A';
+            const char RHS = 'B';
+
+            Action check = () => Guard.That(LHS).IsGreaterThan(RHS, string.Empty);
+
+            check.ShouldThrow<ArgumentException>()
+                .WithMessage(ExpectedCustomeInvalidErrorMessage("IsGreaterThan"));
+        }
+
+        [TestMethod]
+        public void IsGreaterThanCustomMessageIsWhitespace()
+        {
+            const char LHS = 'A';
+            const char RHS = 'B';
+
+            Action check = () => Guard.That(LHS).IsGreaterThan(RHS, "   ");
+
+            check.ShouldThrow<ArgumentException>()
+                .WithMessage(ExpectedCustomeInvalidErrorMessage("IsGreaterThan"));
+        }
+
+        [TestMethod]
+        public void IsGreaterThanCustomMessageFunc()
+        {
+            const char LHS = 'B';
+            Func<char> RHS = () => 'A';
+
+            var result = Guard.That(LHS).IsGreaterThan(RHS, "Shouldn't see me.").Value;
+
+            result.ShouldBeEquivalentTo(LHS);
+        }
+
+        [TestMethod]
+        public void IsGreaterThanCustomMessageFailedFunc()
+        {
+            const string MESSAGE = "Characters can be greater than, dafauc.";
+
+            const char LHS = 'A';
+            Func<char> RHS = () => 'B';
+
+            Action check = () => Guard.That(LHS).IsGreaterThan(RHS, MESSAGE);
+
+            check.ShouldThrow<ArgumentException>()
+                .WithMessage(MESSAGE);
+        }
+
+        [TestMethod]
+        public void IsGreaterThanCustomMessageIsNullFunc()
+        {
+            const char LHS = 'A';
+            Func<char> RHS = () => 'B';
+
+            Action check = () => Guard.That(LHS).IsGreaterThan(RHS, null);
+
+            check.ShouldThrow<ArgumentException>()
+                .WithMessage(ExpectedCustomeInvalidErrorMessage("IsGreaterThan"));
+        }
+
+        [TestMethod]
+        public void IsGreaterThanCustomMessageIsEmptyStringFunc()
+        {
+            const char LHS = 'A';
+            Func<char> RHS = () => 'B';
+
+            Action check = () => Guard.That(LHS).IsGreaterThan(RHS, string.Empty);
+
+            check.ShouldThrow<ArgumentException>()
+                .WithMessage(ExpectedCustomeInvalidErrorMessage("IsGreaterThan"));
+        }
+
+        [TestMethod]
+        public void IsGreaterThanCustomMessageIsWhitespaceFunc()
+        {
+            const char LHS = 'A';
+            Func<char> RHS = () => 'B';
+
+            Action check = () => Guard.That(LHS).IsGreaterThan(RHS, "   ");
+
+            check.ShouldThrow<ArgumentException>()
+                .WithMessage(ExpectedCustomeInvalidErrorMessage("IsGreaterThan"));
+        }
+
+        #endregion IsGreaterThan
+
+        #region IsLessThan
+
+        [TestMethod]
+        public void IsLessThanThanCustomMessage()
+        {
+            const char LHS = 'Y';
+            const char RHS = 'Z';
+
+            var result = Guard.That(LHS).IsLessThan(RHS, "Shouldn't see me.").Value;
+
+            result.ShouldBeEquivalentTo(LHS);
+        }
+
+        [TestMethod]
+        public void IsLessThanustomMessageFailed()
+        {
+            const string MESSAGE = "Characters can be less than, dafauc.";
+
+            const char LHS = 'Z';
+            const char RHS = 'Y';
+
+            Action check = () => Guard.That(LHS).IsLessThan(RHS, MESSAGE);
+
+            check.ShouldThrow<ArgumentException>()
+                .WithMessage(MESSAGE);
+        }
+
+        [TestMethod]
+        public void IsLessThanCustomMessageIsNull()
+        {
+            const char LHS = 'Y';
+            const char RHS = 'Z';
+
+            Action check = () => Guard.That(LHS).IsLessThan(RHS, null);
+
+            check.ShouldThrow<ArgumentException>()
+                .WithMessage(ExpectedCustomeInvalidErrorMessage("IsLessThan"));
+        }
+
+        [TestMethod]
+        public void IsLessThanCustomMessageIsEmptyString()
+        {
+            const char LHS = 'Y';
+            const char RHS = 'Z';
+
+            Action check = () => Guard.That(LHS).IsLessThan(RHS, string.Empty);
+
+            check.ShouldThrow<ArgumentException>()
+                .WithMessage(ExpectedCustomeInvalidErrorMessage("IsLessThan"));
+        }
+
+        [TestMethod]
+        public void IsLessThanCustomMessageIsWhitespace()
+        {
+            const char LHS = 'Y';
+            const char RHS = 'Z';
+
+            Action check = () => Guard.That(LHS).IsLessThan(RHS, "   ");
+
+            check.ShouldThrow<ArgumentException>()
+                .WithMessage(ExpectedCustomeInvalidErrorMessage("IsLessThan"));
+        }
+
+        [TestMethod]
+        public void IsLessThanCustomMessageFunc()
+        {
+            const char LHS = 'Y';
+            Func<char> RHS = () => 'Z';
+
+            var result = Guard.That(LHS).IsLessThan(RHS, "Shouldn't see me.").Value;
+
+            result.ShouldBeEquivalentTo(LHS);
+        }
+
+        [TestMethod]
+        public void IsLessThanCustomMessageFailedFunc()
+        {
+            const string MESSAGE = "Characters can be greater than, dafauc.";
+
+            const char LHS = 'Z';
+            Func<char> RHS = () => 'Y';
+
+            Action check = () => Guard.That(LHS).IsLessThan(RHS, MESSAGE);
+
+            check.ShouldThrow<ArgumentException>()
+                .WithMessage(MESSAGE);
+        }
+
+        [TestMethod]
+        public void IsLessThanCustomMessageIsNullFunc()
+        {
+            const char LHS = 'Y';
+            Func<char> RHS = () => 'Z';
+
+            Action check = () => Guard.That(LHS).IsLessThan(RHS, null);
+
+            check.ShouldThrow<ArgumentException>()
+                .WithMessage(ExpectedCustomeInvalidErrorMessage("IsLessThan"));
+        }
+
+        [TestMethod]
+        public void IsLessThanCustomMessageIsEmptyStringFunc()
+        {
+            const char LHS = 'Y';
+            Func<char> RHS = () => 'Z';
+
+            Action check = () => Guard.That(LHS).IsLessThan(RHS, string.Empty);
+
+            check.ShouldThrow<ArgumentException>()
+                .WithMessage(ExpectedCustomeInvalidErrorMessage("IsLessThan"));
+        }
+
+        [TestMethod]
+        public void IsLessThanCustomMessageIsWhitespaceFunc()
+        {
+            const char LHS = 'Y';
+            Func<char> RHS = () => 'Z';
+
+            Action check = () => Guard.That(LHS).IsLessThan(RHS, "   ");
+
+            check.ShouldThrow<ArgumentException>()
+                .WithMessage(ExpectedCustomeInvalidErrorMessage("IsLessThan"));
+        }
+
+        #endregion IsLessThan
+
+        #region IsInRange
+
+
+
+        #endregion IsInRange
+    }
+}
---
+++ CodeGuardExtensions/CubicleJockey.CodeGuardExtensions/Letters/CharExtensions.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using CubicleJockey.CodeGuardExtensions.Helpers;
+using Seterlund.CodeGuard;
+
+namespace CubicleJockey.CodeGuardExtensions
+{
+    public static class CharExtensions
+    {
+        /// <summary>
+        /// Extension method to IsEqual which allows a custom message.
+        /// </summary>
+        /// <param name="arg">Self</param>
+        /// <param name="param">Value to compare</param>
+        /// <param name="message">The Custom Message</param>
+        /// <returns>Self</returns>
+        public static IArg<char> IsEqual(this IArg<char> arg, char param, string message)
+        {
+            InternalHelpers.IsCustomMessageValid(message, nameof(IsEqual));
+            try
+            {
+                Guard.That(arg.Value).IsEqual(param);
+            }
+            catch(Exception)
+            {
+                arg.Message.Set(message);
+            }
+            return arg;
+        }
+
+        /// <summary>
+        /// Extension method to IsNotEqual which allows a custom message.
+        /// </summary>
+        /// <param name="arg">Self</param>
+        /// <param name="param">Value to compare</param>
+        /// <param name="message">The Custom Message</param>
+        /// <returns>Self</returns>
+        public static IArg<char> IsNotEqual(this IArg<char> arg, char param, string message)
+        {
+            InternalHelpers.IsCustomMessageValid(message, nameof(IsNotEqual));
+            try
+            {
+                Guard.That(arg.Value).IsNotEqual(param);
+            }
+            catch (Exception)
+            {
+                arg.Message.Set(message);
+            }
+            return arg;
+        }
+
+        /// <summary>
+        /// Extension method to IsGreaterThan which allows a custom message.
+        /// </summary>
+        /// <param name="arg">Self</param>
+        /// <param name="param">Value to compare</param>
+        /// <param name="message">The Custom Message</param>
+        /// <returns>Self</returns>
+        public static IArg<char> IsGreaterThan(this IArg<char> arg, char param, string message)
+        {
+            InternalHelpers.IsCustomMessageValid(message, nameof(IsGreaterThan));
+            try
+            {
+                Guard.That(arg.Value).IsGreaterThan(param);
+            }
+            catch(Exception)
+            {
+                arg.Message.Set(message);
+            }
+            return arg;
+        }
+
+        /// <summary>
+        /// Extension method to IsGreaterThan which allows a custom message.
+        /// </summary>
+        /// <param name="arg">Self</param>
+        /// <param name="param">Function with return value to compare</param>
+        /// <param name="message">The Custom Message</param>
+        /// <returns>Self</returns>
+        public static IArg<char> IsGreaterThan(this IArg<char> arg, Func<char> param, string message)
+        {
+            InternalHelpers.IsCustomMessageValid(message, nameof(IsGreaterThan));
+            try
+            {
+                Guard.That(arg.Value).IsGreaterThan(param);
+            }
+            catch (Exception)
+            {
+                arg.Message.Set(message);
+            }
+            return arg;
+        }
+
+        /// <summary>
+        /// Extension method to IsLessThan which allows a custom message.
+        /// </summary>
+        /// <param name="arg">Self</param>
+        /// <param name="param">Value to compare</param>
+        /// <param name="message">The Custom Message</param>
+        /// <returns>Self</returns>
+        public static IArg<char> IsLessThan(this IArg<char> arg, char param, string message)
+        {
+            InternalHelpers.IsCustomMessageValid(message, nameof(IsLessThan));
+            try
+            {
+                Guard.That(arg.Value).IsLessThan(param);
+            }
+            catch(Exception)
+            {
+                arg.Message.Set(message);
+            }
+            return arg;
+        }
+
+        /// <summary>
+        /// Extension method to IsLessThan which allows a custom message.
+        /// </summary>
+        /// <param name="arg">Self</param>
+        /// <param name="param">Function with return value to compare</param>
+        /// <param name="message">The Custom Message</param>
+        /// <returns>Self</returns>
+        public static IArg<char> IsLessThan(this IArg<char> arg, Func<char> param, string message)
+        {
+            InternalHelpers.IsCustomMessageValid(message, nameof(IsLessThan));
+            try
+            {
+                Guard.That(arg.Value).IsLessThan(param);
+            }
+            catch (Exception)
+            {
+                arg.Message.Set(message);
+            }
+            return arg;
+        }
+
+        ///// <summary>
+        ///// Extension method to IsLessThan which allows a custom message.
+        ///// </summary>
+        ///// <param name="arg">Self</param>
+        ///// <param name="param">Value to compare</param>
+        ///// <param name="message">The Custom Message</param>
+        ///// <returns>Self</returns>
+        //public static IArg<char> IsInRange(this IArg<char> arg, char start, char end, string message)
+        //{
+        //    InternalHelpers.IsCustomMessageValid(message, nameof(IsInRange));
+        //    try
+        //    {
+        //        Guard.That(arg.Value).IsInRange()
+        //    }
+        //    catch(Exception)
+        //    {
+        //        arg.Message.Set(message);
+        //    }
+        //    return arg;
+        //}
+    }
+}
